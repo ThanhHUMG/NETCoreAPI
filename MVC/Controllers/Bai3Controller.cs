c6636ffe1817804ec4e1b1bd24c7ab44d1cc6666
ThanhHUMG <snacksa1stpro@gmail.com>
2025-08-30 12:14:02 +0700

    sửa bài 3

--- MVC/Controllers/Bai3Controller.cs
+++ MVC/Controllers/Bai3Controller.cs
@@ -19,26 +19,27 @@ namespace MVC.Controllers
         [HttpPost]
         public IActionResult Index(Bai3 b3)
         {
+            b3.ChieuCao = b3.ChieuCao / 100;
             b3.BMI = b3.CanNang / (b3.ChieuCao * b3.ChieuCao);
 
             if (b3.BMI < 18.5)
             {
-                ViewBag.Message = "Gầy";
+                ViewBag.Message = "Chỉ Số BMI:" + b3.BMI + " -> Gầy";
             }
             else if (b3.BMI < 25)
             {
-                ViewBag.Message = "Bình thường";
+                ViewBag.Message = "Chỉ Số BMI:" + b3.BMI + " -> Bình thường";
             }
             else if (b3.BMI < 30)
             {
-                ViewBag.Message = "Thừa cân";
+                ViewBag.Message = "Chỉ Số BMI:" + b3.BMI + " -> Thừa cân";
             }
             else
             {
-                ViewBag.Message = "Béo phì";
+                ViewBag.Message = "Chỉ Số BMI:" + b3.BMI + " -> Béo phì";
             }
 
-            return View(); 
+            return View(b3); 
         }
     }
 }
--- MVC/obj/Debug/net9.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net9.0/MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe45ab8e9f4d2004bf1260788324ae86df16a864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26169f6569407090cf1d078687ae542a03981f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
